874243bdff59bcede7ed89cd573d22258dd5dd96
ravindrapv <ravindra.ravi4849@gmail.com>
2021-11-08 11:22:54 +0530

    arrays

--- block-BJaacf/code/index.js
+++ block-BJaacf/code/index.js
@@ -1,6 +1,8 @@
 // 1. Create an array named numbers and store 5 number values in it
-
+let numbers = [23,25,28,30,32];
+numbers = 23+25+28+30+32;
 // 2. Calculate the sum of array items and print it to the console using console.log()
+console.log(numbers);
 
 // 3. Calculate the average of array items and print it to the console using console.log()
 
@@ -9,11 +11,26 @@
 // 5. Find the lowest number in the array and print it to the console using console.log()
 
 // 6. Find the even numbers in the array and print them to the console using console.log()
-
+for(let number of numbers) {
+    if(numbers %2 === 0){
+        even.push(numbers);
+    }else {
+        onloadeddata.push(numbers);
+    }
+}
+console.log(even.push);
 // 7. Find the odd numbers in the array and print them to the console using console.log()
-
+for(let number of numbers) {
+    if(numbers %2 === 0){
+        even.push(numbers);
+    }else {
+        onloadeddata.push(numbers);
+    }
+}
+console.log(odd.push);
 // 8. Find the numbers in the array that is divisible by 5 and print them to the console using console.log()
-
+numbers%5;
+console.log(numbers);
 // 9. Log all the element of the array one by one
-
+console.log(numbers);
 // 10. Find all the number in the array that is divisible by 3
